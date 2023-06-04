c26b0f90512171d85abca1256c45a0f8938dff3f
0xf2y <67473845+0xf2y@users.noreply.github.com>
2023-06-04 20:25:21 +0300

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,19 @@
+# Configure the provider software version
+terraform {
+  backend "s3" {
+  bucket = "2tier-app-iti-elfeki"
+    dynamodb_table = "lock_table_name"
+  key    = "file1"
+  region = "us-east-1"
+  encrypt = true
+  access_key = ""
+  secret_key = ""
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  access_key = ""
+  secret_key = ""
+}
